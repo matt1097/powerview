47ced5f6b70bbebe2156216d9dcaf32864063fec
matt1097 <48395861+matt1097@users.noreply.github.com>
2020-03-25 20:31:33 -0400

    Fix scene API to work on v2 hubs. Tested only on v1 hub.

--- powerview-driver_scene.groovy
+++ powerview-driver_scene.groovy
@@ -122,7 +122,7 @@ def updated() {
 // implement the momentary method
 def push() {
 	log.debug("CMD push(${state.pvSceneId})")
-    sendRequest("GET", "/api/scenes?sceneid=${state.pvSceneId}")
+    sendRequest("GET", "/api/scenes?sceneId=${state.pvSceneId}")
 }
 
 def on() {
